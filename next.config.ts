fef569003060aa55a685f7908b85c77f9476f27b
Rahulsaini356 <168537033+Rahulsaini356@users.noreply.github.com>
2026-09-02 22:16:09 +0530

    Set unoptimized images to prevent serverless function credit exhaustion

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
 
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
